74f0faa8a3887ec20fea7c5e995b1263a37d62a4
Ramiro <ramiro.zello@hotmail.com>
2024-02-18 13:44:10 -0300

    Ajustando Componente de Input

--- src/components/Spinner/spinner.tsx
+++ src/components/Spinner/spinner.tsx
@@ -2,8 +2,8 @@ import { AiOutlineLoading } from "react-icons/ai";
 
 export function Spinner() {
   return (
-    <div className="flex items-center justify-center overflow-hidden">
-      <AiOutlineLoading className="font-bold animate-spin text-base-background" />
+    <div className=" items-center justify-center overflow-hidden">
+      <AiOutlineLoading className="font-bold animate-spin text-white" size={24} />
     </div>
   )
 }
