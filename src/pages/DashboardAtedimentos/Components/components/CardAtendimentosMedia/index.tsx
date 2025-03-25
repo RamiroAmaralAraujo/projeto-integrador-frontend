a6e0a487fb8134da6d685567e9883209141e75a6
Almir Pereira Duarte Neto <almirpdneto@gmail.com>
2025-03-25 15:58:50 -0300

    Adicionado ícones diferentes dependendo da média de atendimento & alterado nome do card de média de atendimentos

--- src/pages/DashboardAtedimentos/Components/components/CardAtendimentosMedia/index.tsx
+++ src/pages/DashboardAtedimentos/Components/components/CardAtendimentosMedia/index.tsx
@@ -1,5 +1,5 @@
 import { useRead } from "@/hook/queries/useAtendimentos";
-import { MessageSquareMore } from "lucide-react";
+import { MessageSquareMore, Angry, Frown, Meh, Smile, Laugh } from "lucide-react";
 
 export function CardAtendimentosMedia() {
   const { data: atendimentos } = useRead();
@@ -22,18 +22,28 @@ export function CardAtendimentosMedia() {
     return "gray";
   };
 
+  // Função para determinar o ícone da média
+  const getMediaIcon = (media: number) => {
+    if (media >= 4.5 && media <= 5) return <Laugh size={30} />;
+    if (media >= 4 && media < 4.5) return <Smile size={30} />;
+    if (media >= 3 && media < 4) return <Meh size={30} />;
+    if (media >= 2 && media < 3) return <Frown size={30} />;
+    if (media >= 1 && media < 2) return <Angry size={30} />;
+    return <MessageSquareMore size={30} />;
+  };
+
   return (
     <>
       <div className="bg-gray-50 w-full rounded-xl shadow-lg p-6 mb-4">
         <h1 className="font-bold text-brand-blue-500 text-2xl">
-          Média de Atendimentos
+          Atendimentos Média
         </h1>
         <div className="flex items-center justify-between gap-5 mt-5">
           <span className={`text-2xl font-semibold text-${getMediaColor(Number(mediaAtendimentos))}-500`}>
             {mediaAtendimentos}
           </span>
           <div className={`w-14 h-14 bg-${getMediaColor(Number(mediaAtendimentos))}-300 rounded-full flex justify-center items-center text-${getMediaColor(Number(mediaAtendimentos))}-700`}>
-            <MessageSquareMore size={30} />
+            {getMediaIcon(Number(mediaAtendimentos))}
           </div>
         </div>
       </div>
