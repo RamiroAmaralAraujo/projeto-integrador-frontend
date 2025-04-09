a58d799d83b60203e1252babc56a8434771c68d7
Ramiro Amaral Araujo <ramiro.zello@hotmail.com>
2025-04-09 17:23:04 -0300

    mudando http para https

--- src/components/MensagensTickets/index.tsx
+++ src/components/MensagensTickets/index.tsx
@@ -23,7 +23,7 @@ export function MensagensTicketForm() {
     };
 
     try {
-      const response = await fetch(`http://core-commerce-api.onrender.com/ticketmensagem`, {
+      const response = await fetch(`https://core-commerce-api.onrender.com/ticketmensagem`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
